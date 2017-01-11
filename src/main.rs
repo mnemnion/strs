78a2f2599a50d51395433a3649671abda20e10d7
Sam Atman <atmanistan@gmail.com>
2017-01-10 18:11:05 -0800

    Adds command-line flags
    Simple definition of the interface here.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 extern crate clap;
 
-use clap::{App};
+use clap::{App, Arg};
 
 
 fn main() {
@@ -8,5 +8,17 @@ fn main() {
             .version("0.0.1 SNAPSHOT")
             .about("\nstrs filters a text stream for quoted strings. Follows C conventions for what a string is.")
             .author("Sam Putman: github/mnemnion")
+            .arg(Arg::with_name("env")
+            	.help("Wraps the strings as [a,b,c]. Joins with ',' by default, may be overridden with -j.")
+            	.long("envelop")
+            	.short("e")
+            	.takes_value(false)
+            	.required(false))
+            .arg(Arg::with_name("join")
+                .help("Join the strings with <join>. Defaults to \" \" or \",\" with -e.")
+                .long("join")
+                .short("j")
+                .takes_value(true)
+                .required(false))
             .get_matches();
     }
