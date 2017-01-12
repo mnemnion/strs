62f7cebe6aa156fa5aa93ed4420592c31539fb7a
Sam Atman <atmanistan@gmail.com>
2017-01-12 15:39:28 -0800

    A (possible) answer to handling input
    Rust is hard, let's go shopping.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,51 @@
 extern crate clap;
 
 use clap::{App, Arg, ArgMatches};
+use std::io::{self, Read, BufRead, stdin};
+use std::fs::File;
 
+struct Input<'a> {
+    source: Box<BufRead + 'a>
+}
+
+impl<'a> Input<'a> {
+    fn console(stdin: &'a io::Stdin) -> Input<'a> {
+        Input { source: Box::new(stdin.lock()) }
+    }
+
+    fn file(path: &str) -> io::Result<Input<'a>> {
+        File::open(path)
+            .map(|file| Input { source: Box::new(io::BufReader::new(file)) })
+    }
+}
+
+impl<'a> Read for Input<'a> {
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        self.source.read(buf)
+    }
+}
+
+impl<'a> BufRead for Input<'a> {
+    fn fill_buf(&mut self) -> io::Result<&[u8]> {
+        self.source.fill_buf()
+    }
+    fn consume(&mut self, amt: usize) {
+        self.source.consume(amt);
+    }
+}
+
+
+/// Parse the command line arguments.
 fn getOpts<'a>() -> ArgMatches<'a> {
     App::new("strs")
-            .version("0.0.1 SNAPSHOT")
+            .version("0.1.0 SNAPSHOT")
             .about("\nstrs filters a text stream for quoted strings, passing the result to stdout. \
                    Follows C conventions for what a string is.")
             .author("Sam Putman: github/mnemnion")
             .arg(Arg::with_name("env")
-                .help("Wraps the strings as [a,b,c]. Joins with ',' by default. May be overridden with -j.")
+                .help("Wraps the strings as [str1,...,strN]. \
+                       Joins with ',' by default. \
+                       May be overridden with -j.")
                 .long("envelop")
                 .short("e")
                 .takes_value(false)
@@ -47,5 +83,7 @@ fn getOpts<'a>() -> ArgMatches<'a> {
 
 
 fn main() {
-    let matches = getOpts();
+    let matches = getOpts(); 
+    let standard = stdin();
+    let stream = Input::console(&standard); 
     }
